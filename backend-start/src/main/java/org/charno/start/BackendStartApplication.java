eb7ccafc8c25a542103d51b540d9d0ce01078d76
死鱼安乐 <aa928531940@outlook.com>
2025-12-16 02:56:31 +0800

    First commit

---
+++ backend-start/src/main/java/org/charno/start/BackendStartApplication.java
@@ -0,0 +1,13 @@
+package org.charno.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStartApplication.class, args);
+    }
+
+}
